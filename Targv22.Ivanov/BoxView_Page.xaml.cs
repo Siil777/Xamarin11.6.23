67f632dd6ae621b6252ca30cb45346dec2f8e01f
Siil777 <pavel.ivanov.thk@gmail.com>
2023-11-08 21:09:27 +0200

    Xamarin 1.Box_ViewPage
    1. Class Constructor: inside the BoxView_Page class, define a constructor BoxView_Page()
    
    2.BoxView Initialization: box = new BoxView {}
    
    3.StackLayout and Content: StackLayout named st and add the box to it. The StackLayout is set as the content of the page using Content = st.
    
    4. Create a label with some text:  label = new Label {}
    
    5.Event Handlers: Event handlers (Tap_Tapped, C_Clicked, D_Clicked) are defined for the buttons and box.
    
    6.  TapGestureRecognizer tap = new TapGestureRecognizer();
     tap.Tapped += Tap_Tapped;
     box.GestureRecognizers.Add(tap);  is used to detect tap (or click) events on a user interface element like a BoxView.

--- Targv22.Ivanov/BoxView_Page.xaml.cs
+++ Targv22.Ivanov/BoxView_Page.xaml.cs
@@ -9,59 +9,133 @@ using Xamarin.Forms.Xaml;
 namespace Targv22.Ivanov
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class BoxView_Page : ContentPage 
+    public partial class BoxView_Page : ContentPage
 
-        
 
-       
+
+
+
     {
         BoxView box;
         Label label;
 
+
         public BoxView_Page()
         {
+            rnd = new Random();
+
             int r = 0, g = 0, b = 0;
+
+
+
             box = new BoxView
             {
-                Color = Color.FromRgb(r,g,b),
-                CornerRadius=10,
-                WidthRequest=200, HeightRequest=400,
-                HorizontalOptions=LayoutOptions.Center,
-                VerticalOptions=LayoutOptions.CenterAndExpand
+                Color = Color.FromRgb(r, g, b),
+                CornerRadius = rnd.Next(1, 10),
+                WidthRequest = rnd.Next(1, 200),
+                HeightRequest = rnd.Next(1, 400),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+
 
 
             };
-          
-            TapGestureRecognizer tap= new TapGestureRecognizer();
-            tap.Tapped += Tap_Tapped; ;
-            box.GestureRecognizers.Add(tap);
-            StackLayout st=new StackLayout { Children = {box} };
-            Content= st;
 
             label = new Label
             {
                 Text = "Pealkiri",
                 HorizontalOptions = LayoutOptions.Start,
                 VerticalOptions = LayoutOptions.Center,
-                TextColor = Color.Black,
-                BackgroundColor = Color.White,
+                TextColor = Color.Green,
+                BackgroundColor = Color.Black,
+                FontSize = 24
 
             };
-            
+
+
+            TapGestureRecognizer tap = new TapGestureRecognizer();
+            tap.Tapped += Tap_Tapped;
+            box.GestureRecognizers.Add(tap);
+
+
+            //button to reset count of numbers
+            Button c = new Button
+            {
+                Text = "Restart count",
+                HorizontalOptions = LayoutOptions.End,
+                VerticalOptions = LayoutOptions.Center,
+
+            };
+            //button to go back
+            Button d = new Button
+            {
+
+                Text = "Back",
+                HorizontalOptions = LayoutOptions.StartAndExpand,
+                VerticalOptions = LayoutOptions.Center,
+
+
+            };
+
+            c.Clicked += C_Clicked;
+            d.Clicked += D_Clicked;
+
+            StackLayout st = new StackLayout { Children = { label, box, c, d } };
+            BackgroundColor = Color.White;
+            Content = st;
+
+
         }
+
+        private async void D_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
         Random rnd;
-        int i= 0;
+        int tapCount = 0;
         private void Tap_Tapped(object sender, EventArgs e)
         {
-            rnd= new Random();
-            box.Color=Color.FromRgb(rnd.Next(0,255), rnd.Next(0,255),
-                rnd.Next(0,255));
+            
 
-           
+            tapCount++;
+            label.Text = "Taps: " + tapCount;
+
+
+
+            rnd = new Random();
+            box.Color = Color.FromRgb(rnd.Next(0, 255), rnd.Next(0, 255),
+                rnd.Next(0, 255));
+
+
+            box.CornerRadius = rnd.Next(1, 10);
+            box.WidthRequest = rnd.Next(1, 200);
+            box.HeightRequest = rnd.Next(1, 400);
 
 
 
         }
-        
-}
+
+        private void C_Clicked(object sender, EventArgs e)
+        {
+            ResetShapeProperties();
+
+
+            tapCount = 0;
+            label.Text = "Taps: " + tapCount;
+
+
+        }
+        private void ResetShapeProperties()
+        {
+            // Reset the shape's properties to their initial values
+            int r = 0, g = 0, b = 0;
+            box.Color = Color.FromRgb(r, g, b);
+            box.CornerRadius = rnd.Next(1, 10);
+            box.WidthRequest = rnd.Next(1, 200);
+            box.HeightRequest = rnd.Next(1, 400);
+        }
+    }
+
+
 }
