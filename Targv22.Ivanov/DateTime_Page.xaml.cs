351955417bfb7f8aed2c2fa765349c23f76fbc6b
Siil777 <pavel.ivanov.thk@gmail.com>
2023-11-10 16:26:55 +0200

    Xamarin 1.2 DateTime Page
    1. DatePicker MinimumDate=DateTime.Now.AddDays(-5), is set to display the date in the "D" (long date) format and restricted to a range of five days before and after the current date.
    
    2. TimePicker Time = new TimeSpan(12,0,0),  is initialized with a default time of 12:00 PM.
    
    3. DateSelected event of the DatePicker (dp). When the date is selected, the Dp_DateSelected method is called.
    
    4. PropertyChanged event of the TimePicker (tp). When the time is changed, the Tp_PropertyChanged method is called, updating the label text with the selected time.
    
    5. AbsoluteLayout named abs to position the UI elements
    
    6. AbsoluteLayout.SetLayoutBounds and AbsoluteLayout.SetLayoutFlags to specify the position and size of the Label
    
    7. Tp_PropertyChanged method, you update the label text with the selected time
    
    8. Dp_DateSelected method, you update the label text with the selected date.

---
+++ Targv22.Ivanov/DateTime_Page.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Targv22.Ivanov
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DtaeTime_Page : ContentPage
+    {
+        //Storing these variables at the class level to make them accessible throughout the entire class
+        Label lbl;
+        DatePicker dp;
+        TimePicker tp;
+
+        public DtaeTime_Page()
+        {
+            //Initialization of UI Elements
+            lbl = new Label
+            {
+                Text = "Vali mingi kuupäev",
+                
+                BackgroundColor= Color.Chocolate,
+
+
+            };
+            //Initialization of UI Elements
+            dp = new DatePicker
+            {
+                Format = "D",
+                MinimumDate=DateTime.Now.AddDays(-5),
+                MaximumDate=DateTime.Now.AddDays(5),
+                TextColor= Color.Cornsilk,
+
+            };
+            dp.DateSelected += Dp_DateSelected;
+
+            //Initialization of UI Elements
+            tp = new TimePicker
+            {
+                Time = new TimeSpan(12,0,0),
+            };
+            tp.PropertyChanged += Tp_PropertyChanged;
+
+
+
+            AbsoluteLayout abs = new AbsoluteLayout
+            {
+                Children = { lbl, dp, tp }
+
+            };
+
+            //AbsoluteLayout.SetLayoutBounds and AbsoluteLayout.SetLayoutFlags to specify the position and size of the Label
+            AbsoluteLayout.SetLayoutBounds(lbl, new Rectangle(0.1, 0.2, 200, 50));
+            AbsoluteLayout.SetLayoutFlags(lbl, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(lbl, new Rectangle(0.1, 0.5, 300, 50));
+            AbsoluteLayout.SetLayoutFlags(lbl, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(lbl, new Rectangle(0.5, 0.7, 300, 50));
+            AbsoluteLayout.SetLayoutFlags(lbl, AbsoluteLayoutFlags.PositionProportional);
+
+
+            Content = abs;
+
+        }
+
+
+
+        //Event Handling PropertyChanged event of the TimePicker (tp). When the time is changed, the
+        //Tp_PropertyChanged method is called, updating the label text with the selected time.
+
+        private void Tp_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            lbl.Text = "Oli valitud aeg: " + tp.Time;
+            lbl.FontSize = 20;
+
+        }
+        //Event Handling DateSelected event of the DatePicker (dp). When the date is selected,
+        //the Dp_DateSelected method is called, updating the label text with the selected date
+        private void Dp_DateSelected(object sender, DateChangedEventArgs e)
+        {
+
+            lbl.Text = "Oli valitud kuupäev" + e.NewDate.ToString("G");
+            lbl.FontSize = 20;
+
+        }
+      
+
+        
+       
+    }
+    
+}
--- Targv22.Ivanov/Startpage.xaml.cs
+++ Targv22.Ivanov/Startpage.xaml.cs
@@ -29,12 +29,18 @@ namespace Targv22.Ivanov
                 Text = "View Box",
                 BackgroundColor = Color.Azure
             };
+            Button Date_Time_btn = new Button
+            {
+                Text="DateTime Page",
+                BackgroundColor = Color.Azure
+
+            };
 
 
             StackLayout st =new StackLayout
             {
                 //all elements on the page pictures, buttons...call them from the structure above Button Ent_btn=new Button, Button Time_btn = new Button
-                Children = {Ent_btn, Time_btn, VievBox_btn},
+                Children = {Ent_btn, Time_btn, VievBox_btn, Date_Time_btn },
                 BackgroundColor=Color.Beige
 
 
@@ -44,8 +50,15 @@ namespace Targv22.Ivanov
             Ent_btn.Clicked += Ent_btn_Clicked;
             Time_btn.Clicked += Time_btn_Clicked;
             VievBox_btn.Clicked += VievBox_btn_Clicked;
+            Date_Time_btn.Clicked += Date_Time_btn_Clicked;
 
 
+        }
+
+        //Display button in app navigation
+        private async void Date_Time_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DtaeTime_Page());
         }
 
         private async void VievBox_btn_Clicked(object sender, EventArgs e)
