46c9df23c598cda96d23f2bdddc71603a8947975
Siil777 <pavel.ivanov.thk@gmail.com>
2023-11-30 14:15:40 +0200

    Xamarin 1.7 Table_Page,sms,call,email

--- Targv22.Ivanov/Startpage.xaml.cs
+++ Targv22.Ivanov/Startpage.xaml.cs
@@ -54,19 +54,32 @@ namespace Targv22.Ivanov
                 BackgroundColor=Color.Azure
 
             };
-            Button pickerWeb_btn = new Button
+         
+            Button Table_page_btn = new Button
             {
-                Text="Internet",
-                 BackgroundColor = Color.Azure
+                Text = "Table page",
+                BackgroundColor = Color.Azure
 
             };
-           
+            Button Table_View_Page = new Button
+            {
+                Text = "Table page 1",
+                BackgroundColor = Color.Azure
+
+            };
+            Button Picker_Internet = new Button
+            {
+                Text = "Picker Internet",
+                BackgroundColor = Color.Azure
+
+            };
+
 
 
             StackLayout st =new StackLayout
             {
                 //all elements on the page pictures, buttons...call them from the structure above Button Ent_btn=new Button, Button Time_btn = new Button
-                Children = {Ent_btn, Time_btn, VievBox_btn, Date_Time_btn, StepperSlider_btn, Frame_btn, valgusfor_btn, pickerWeb_btn },
+                Children = {Ent_btn, Time_btn, VievBox_btn, Date_Time_btn, StepperSlider_btn, Frame_btn, valgusfor_btn, Table_page_btn, Table_View_Page, Picker_Internet },
                 BackgroundColor=Color.Beige
 
 
@@ -80,10 +93,19 @@ namespace Targv22.Ivanov
             StepperSlider_btn.Clicked += StepperSlider_btn_Clicked;
             Frame_btn.Clicked += Frame_btn_Clicked;
             valgusfor_btn.Clicked += Valgusfor_btn_Clicked;
-            pickerWeb_btn.Clicked += PickerWeb_btn_Clicked;
             
+            Table_page_btn.Clicked += Table_page_btn_Clicked;
+            Table_View_Page.Clicked += Table_View_Page_Clicked;
+            Picker_Internet.Clicked += Picker_Internet_Clicked;
 
 
+
+
+        }
+
+        private async void Picker_Internet_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Picker_Internet());
         }
 
         private async void Table_View_Page_Clicked(object sender, EventArgs e)
@@ -94,12 +116,6 @@ namespace Targv22.Ivanov
         private async void Table_page_btn_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Table_Page());
-
-        }
-
-        private async void PickerWeb_btn_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new Picker_Page());
 
         }
 
